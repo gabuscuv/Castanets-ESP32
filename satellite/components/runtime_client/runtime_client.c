09831603333006539b1fde94479b433396d81f2d
Gabriel Bustillo del Cuvillo <52936877+gabuscuv@users.noreply.github.com>
2026-08-20 17:58:42 +0200

    [satellite/satellite_client] Runtime_Client Callback Implementation (#2)
    * WIP
    
    * Fixed some general Issues
    
    * WIP (Part 2)
    
    * Restoring Code (This concerns to heartbeat handling)

--- satellite/components/runtime_client/runtime_client.c
+++ satellite/components/runtime_client/runtime_client.c
@@ -1,9 +1,9 @@
 #include "runtime_client.h"
+#include "esp_err.h"
 #include "esp_log.h"
 #include "piezocontroller.h"
 #include "satellite_client.h"
 #include "ledcontroller.h"
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
@@ -11,9 +11,23 @@ static const char *TAG = "runtime_client";
 
 static uint32_t s_time;
 
-static void runtime_client_time_callback(uint32_t time)
+static esp_err_t runtime_client_callback(satellite_message_runtime_t msg)
 {
-    s_time = time; 
+  switch (msg.type) {
+
+    case CONTROLLER_CMD_SET_GAME_TIME:
+        s_time = msg.time;
+        break;
+    case CONTROLLER_CMD_RESET_TIMEHUB:
+        s_time = 0;
+        break;
+    case CONTROLLER_ACK_ROLE:
+    case CONTROLLER_CMD_START_SONG:
+    
+    case CONTROLLER_CMD_REQUEST_STATUS:
+      break;
+    }
+    return ESP_OK;
 }
 
 static void runtime_piezo_callback(void)
@@ -45,7 +59,7 @@ esp_err_t runtime_client_init(void)
     if (err != ESP_OK){return err;}
 
     ESP_LOGI(TAG, "Starting Satellite Client");
-    err = satellite_client_init(runtime_client_time_callback);
+    err = satellite_client_init(runtime_client_callback);
     if (err != ESP_OK){return err;}
 
     ESP_LOGI(TAG, "Intializing Piezo Controller");
